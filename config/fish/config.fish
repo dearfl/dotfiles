49026bb1894b26e70b1d88a2a72007ac07aaf9a5
dearfl <i@flr.me>
2022-09-12 23:02:19 +0800

    <update> add zoxide fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x GPG_TTY (tty)
     set -g __fish_git_prompt_char_dirtystate '+'
+    zoxide init fish | source
 end
