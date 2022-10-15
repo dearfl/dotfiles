f1d48d7c623af8ca719a891b6568d805285d155c
dearfl <i@flr.me>
2022-10-15 10:00:30 +0800

    <update> change TERM var to fix alacritty

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x GPG_TTY (tty)
+    set -x TERM xterm-256color
     set -g __fish_git_prompt_char_dirtystate '+'
     zoxide init fish | source
 end
